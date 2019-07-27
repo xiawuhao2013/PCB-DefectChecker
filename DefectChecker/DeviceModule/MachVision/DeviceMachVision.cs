f73e95a161098739e833661002d748ea6a5a125c
xiawuhao2013 <xiawuhao2011@foxmail.com>
2019-07-27 11:39:51 +0800

    Mach# little change.

--- DefectChecker/DeviceModule/MachVision/DeviceMachVision.cs
+++ DefectChecker/DeviceModule/MachVision/DeviceMachVision.cs
@@ -1,21 +1,94 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+using Aqrose.Framework.Utility.Tools;
 using DefectChecker.DefectDataStructure;
+
+using PathMap = System.Collections.Generic.Dictionary<string, string>;
 
 namespace DefectChecker.DeviceModule.MachVision
 {
     class DeviceMachVision : DeviceInterface
     {
-        public void GetBatchList(out List<string> batchList)
+        private const string _paramFileName = @"\ParamFile.xml";
+        private string _dataDir = default(string);
+        private string _modelDir = default(string);
+        private PathMap _productMap = new PathMap();
+        private PathMap _batchMap = new PathMap();
+        private PathMap _boardMap = new PathMap();
+
+        DeviceMachVision()
         {
-            throw new NotImplementedException();
+            LoadConfig();
         }
 
-        public void GetBoardList(string batchName, out List<string> boardList)
+        private bool GetChildrenDirMap(string dirPath, out PathMap childrenDirMap)
+        {
+            childrenDirMap = new PathMap();
+            //childrenDirMap.Clear();
+            try
+            {
+                var dir = new DirectoryInfo(dirPath);
+                foreach (var dirInfo in dir.GetDirectories())
+                {
+                    childrenDirMap.Add(dirInfo.Name, dirInfo.FullName);
+                }
+            }
+            catch (Exception ex)
+            {
+                childrenDirMap.Clear();
+                MessageBox.Show(ex.Message);
+
+                return false;
+            }
+
+            return true;
+        }
+
+        private void LoadConfig()
+        {
+            XmlParameter xmlParameter = new XmlParameter();
+            xmlParameter.ReadParameter(Application.StartupPath + _paramFileName);
+            _dataDir = xmlParameter.GetParamData("DataDir");
+            _modelDir = xmlParameter.GetParamData("ModelDir");
+
+            return;
+        }
+
+        public void GetBatchList(string productName, out List<string> batchList)
+        {
+            batchList = new List<string>();
+            //batchList.Clear();
+            try
+            {
+                GetProductList(out var procductList);
+                if (!_productMap.TryGetValue(productName, out var productPath))
+                {
+                    return;
+                }
+                if (!GetChildrenDirMap(productPath, out _batchMap))
+                {
+                    return;
+                }
+                foreach (var batch in _batchMap)
+                {
+                    batchList.Add(batch.Key);
+                }
+            }
+            catch (Exception ex)
+            {
+                batchList.Clear();
+                MessageBox.Show(ex.Message);
+
+                return;
+            }
+
+            return;
+        }
+
+        public void GetBoardList(string productName, string batchName, out List<string> boardList)
         {
             throw new NotImplementedException();
         }
@@ -30,17 +103,53 @@ namespace DefectChecker.DeviceModule.MachVision
             throw new NotImplementedException();
         }
 
-        public void GetDefetInBoard(string batchName, string boardName, out List<string> defectImgList)
+        public void GetDefetInBoard(string productName, string batchName, string boardName, out List<string> defectImgList)
         {
             throw new NotImplementedException();
         }
 
-        public void GetGerberWholeImg(out Bitmap gerberWholeImg)
+        public void GetGerberWholeImgA(out Bitmap gerberWholeImg)
         {
             throw new NotImplementedException();
         }
 
-        public void GetTemplateWholeImg(out Bitmap templateWholeImg)
+        public void GetGerberWholeImgB(out Bitmap gerberWholeImg)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetProductList(out List<string> procductList)
+        {
+            procductList = new List<string>();
+            //procductList.Clear();
+            try
+            {
+                if (!GetChildrenDirMap(_dataDir, out _productMap))
+                {
+                    return;
+                }
+                foreach (var product in _productMap)
+                {
+                    procductList.Add(product.Key);
+                }
+            }
+            catch (Exception ex)
+            {
+                procductList.Clear();
+                MessageBox.Show(ex.Message);
+
+                return;
+            }
+
+            return;
+        }
+
+        public void GetTemplateWholeImgA(out Bitmap wholeImg)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetTemplateWholeImgB(out Bitmap wholeImg)
         {
             throw new NotImplementedException();
         }
